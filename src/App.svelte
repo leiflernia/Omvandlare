a50b9b0f3bbead42599dfcb014ca9b01bf19cafe
Leif Johansson <leiflernia@outlook.com>
2022-12-15 10:33:49 +0100

    msk och tsk

--- src/App.svelte
+++ src/App.svelte
@@ -6,33 +6,61 @@
 	let kg = 1;
 	let hg = 10;
 	let g = 1000;
+	let msk = 66.67;
+	let tsk = 200;
 
 	function setfromL(value) {
 		l = +value;
-		dl = +(10 * l);
-		cl = +(100 * l);
-		ml = +(1000 * l);
+		dl = +(10 * l).toFixed(3);
+		cl = +(100 * l).toFixed(3);
+		ml = +(1000 * l).toFixed(3);
+		msk = +(l / 0.015).toFixed(3);
+		tsk = +(l / 0.005).toFixed(3);
 	}
 
 	function setfromDl(value) {
 		dl = +value;
-		l = +(dl / 10);
-		cl = +(100 * l);
-		ml = +(1000 * l);
+		l = +(dl / 10).toFixed(3);
+		cl = +(100 * l).toFixed(3);
+		ml = +(1000 * l).toFixed(3);
+		msk = +(l / 0.015).toFixed(3);
+		tsk = +(l / 0.005).toFixed(3);
 	}
 
 	function setfromCl(value) {
 		cl = +value;
-		l = +(cl / 100);
-		dl = +(10 * l);
-		ml = +(1000 * l);
+		l = +(cl / 100).toFixed(3);
+		dl = +(10 * l).toFixed(3);
+		ml = +(1000 * l).toFixed(3);
+		msk = +(l / 0.015).toFixed(3);
+		tsk = +(l / 0.005).toFixed(3);
 	}
 
 	function setfromMl(value) {
 		ml = +value;
-		l = +(ml / 1000);
-		dl = +(10 * l);
-		ml = +(1000 * l);
+		l = +(ml / 1000).toFixed(3);
+		cl = +(100 * l).toFixed(3);
+		dl = +(10 * l).toFixed(3);
+		msk = +(l / 0.015).toFixed(3);
+		tsk = +(l / 0.005).toFixed(3);
+	}
+
+	function setfromMsk(value) {
+		msk = +value;
+		l = +(msk * 0.015).toFixed(3);
+		dl = +(msk * 0.15).toFixed(3);
+		cl = +(msk * 1.5).toFixed(3);
+		ml = +(msk * 15).toFixed(3);
+		tsk = +(l / 0.005).toFixed(3);
+	}
+
+	function setfromTsk(value) {
+		tsk = +value;
+		l = +(tsk * 0.005).toFixed(3);
+		dl = +(tsk * 0.05).toFixed(3);
+		cl = +(tsk * 0.5).toFixed(3);
+		ml = +(tsk * 5).toFixed(3);
+		msk = +(l / 0.015).toFixed(3);
 	}
 
 	function setfromKg(value) {
@@ -40,11 +68,13 @@
 		hg = +(kg * 10);
 		g = +(kg * 1000);
 	}
+
 	function setfromHg(value) {
 		hg = +value;
 		kg = +(hg / 10);
 		g = +(hg * 100);
 	}
+
 	function setfromG(value) {
 		g = +value;
 		kg = +(g / 1000);
@@ -92,6 +122,24 @@
 			<input
 				value={ml}
 				on:input={(e) => setfromMl(e.target.value)}
+				type="number"
+			/>
+		</div>
+		<div class="matsked">
+			<p>Matsked (msk)</p>
+
+			<input
+				value={msk}
+				on:input={(e) => setfromMsk(e.target.value)}
+				type="number"
+			/>
+		</div>
+		<div class="tesked">
+			<p>Matsked (tsk)</p>
+
+			<input
+				value={tsk}
+				on:input={(e) => setfromTsk(e.target.value)}
 				type="number"
 			/>
 		</div>
@@ -166,6 +214,14 @@
 		flex-basis: 160px;
 	}
 	.milliliter {
+		flex-grow: 1;
+		flex-basis: 160px;
+	}
+	.matsked {
+		flex-grow: 1;
+		flex-basis: 160px;
+	}
+	.tesked {
 		flex-grow: 1;
 		flex-basis: 160px;
 	}
